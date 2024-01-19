66584dc8da981bbf3f54f6733d61506bbfb58cd7
victoriakapelush <victoriakapelush@gmail.com>
2024-01-18 20:36:13 -0500

    Cached files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-<<<<<<< HEAD
-    mongoDB: process.env.MONGO_DB_CONNECTION_STRING,
-  };
-=======
-  mongoDB: process.env.MONGO_DB_CONNECTION_STRING,
-};
->>>>>>> 6762b1aeec7c6c3171794e6757030a4147919589
