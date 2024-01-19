5ea0e4cdecfc7de8708b469c6dc809875babfe7b
Victoria Kapelush <42095608+victoriakapelush@users.noreply.github.com>
2024-01-18 20:22:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var config = {
-  MY_KEY : "mongodb+srv://victoriakapelush:sakuraSun123@cluster0.qpt6ako.mongodb.net/inventory_application?retryWrites=true&w=majority";
-}
+module.exports = {
+  mongoDB: process.env.MONGO_DB_CONNECTION_STRING,
+};
